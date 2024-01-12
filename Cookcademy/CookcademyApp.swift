6204c777c73f38067cd1d3989f8b8e0982789e53
ofek avigdor <ofekag@gmail.com>
2024-01-12 16:26:59 +0200

    first commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//
+//
+//
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainTabView()
+        }
+    }
+}
---
+++ Cookcademy/Views/ExploreRecipes/RecipeCategoryGridView.swift
@@ -0,0 +1,50 @@
+//
+//  RecipeCategoryGridView.swift
+//  Cookcademy
+//
+//
+
+import SwiftUI
+
+struct RecipeCategoryGridView: View {
+    @EnvironmentObject private var recipeData: RecipeData
+
+    var body: some View {
+        let columns = [GridItem(), GridItem()]
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns, content: {
+                    ForEach(MainInformation.Category.allCases, id: \.self) { category in
+                        NavigationLink(
+                            destination: RecipesListView(viewStyle: .singleCategory(category)),
+                            label: {
+                                CategoryView(category: category)
+                            })
+                    }
+                })
+            }
+            .navigationTitle("Categories")
+        }
+    }
+}
+
+struct CategoryView: View {
+    let category: MainInformation.Category
+    
+    var body: some View {
+        ZStack {
+            Image(category.rawValue)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .opacity(0.55)
+            Text(category.rawValue)
+                .font(.title)
+        }
+    }
+}
+
+struct RecipeCategoryGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeCategoryGridView()
+    }
+}
